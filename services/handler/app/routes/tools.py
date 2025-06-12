d9624ce34767cc5dbbc5ad9819e738d8a3a50a1b
gabriel.juraj <gabriel.juraj@gmail.com>
2025-06-12 22:47:18 +0200

    testujeme

--- services/handler/app/routes/tools.py
+++ services/handler/app/routes/tools.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.schemas.tool_request import ToolRequest
+from fastapi.responses import HTMLResponse
 from app.tools.echo import echo_message
 
 router = APIRouter()
@@ -7,3 +8,17 @@ router = APIRouter()
 @router.post("/echo")
 async def run_echo(req: ToolRequest):
     return {"result": echo_message(req.message)}
+
+@router.get("/echo2", response_class=HTMLResponse)
+async def echo():
+    return """
+    <html>
+        <head>
+            <title>Echo Test</title>
+        </head>
+        <body>
+            <h1>👋 Hello from Jenkins FastAPI!</h1>
+            <p>This is a test response in HTML.</p>
+        </body>
+    </html>
+    """
